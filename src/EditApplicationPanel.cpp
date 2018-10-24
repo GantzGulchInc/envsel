58b16041b83307ed94433c6d26a18939a497454a
Michael L. Gantz <gantzm@gantzgulch.com>
2018-10-24 15:46:01 -0400

    Edit panels!

---
+++ src/EditApplicationPanel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gantzm on 10/24/18.
+//
+
+#include "EditApplicationPanel.h"
---
+++ src/EditApplicationPanel.h
@@ -0,0 +1,14 @@
+//
+// Created by gantzm on 10/24/18.
+//
+
+#ifndef ENVSEL_EDITAPPLICATIONPANEL_H
+#define ENVSEL_EDITAPPLICATIONPANEL_H
+
+
+class EditApplicationPanel {
+
+};
+
+
+#endif //ENVSEL_EDITAPPLICATIONPANEL_H
